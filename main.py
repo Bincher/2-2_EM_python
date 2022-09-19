66119417d78deb41d1e00f605ac9e93bd7e4df15
Bincher <98527148+Bincher@users.noreply.github.com>
2022-09-19 10:24:50 +0900

    numpy.array()

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import math
+import numpy as np
 
-def sumToNM(value1, value2):
-    result = 0
-    for n in range(value1, value2 + 1):
-        result += n
-    return result
+arr1 = np.array([3, 5, 8])
+arr1
+arr1[0] = 2
+print(arr1[0], arr1[1], arr1[2])
+print(arr1.ndim)
+print(arr1.shape)
+print(arr1.dtype)
 
-print(sumToNM(5,25))
-print(sumToNM(100,512))
