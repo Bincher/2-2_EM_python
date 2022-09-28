f9c863068e02e0fb12dd9c765ade4a308cb704b1
Bincher <98527148+Bincher@users.noreply.github.com>
2022-09-28 16:05:52 +0900

    공수4주차 - 이분법을 수행하기 위한 M-파일

--- main.py
+++ main.py
@@ -5,44 +5,98 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
 """
-#축 라벨, 제목, 범례 사용(sin,cos 2개 겹치기)
-x = np.arange(0, 3 * np.pi, 0.1)
-y_sin = np.sin(x)
-y_cos = np.cos(x)
-plt.plot(x, y_sin)
-plt.plot(x, y_cos)
-plt.xlabel("x value")
-plt.ylabel("y value")
-plt.title("sine & cosine")
-plt.legend(["sine", "cosine"])
+cd = 0.25
+g = 9.81
+v = 36
+t = 4
+mp = np.linspace(50, 200, 10)
+fp = np.sqrt(g*mp/cd) * np.tanh(np.sqrt(g*cd/mp)*t) - v
+
+plt.title("m graph")
+plt.plot(mp, fp)
+plt.grid(color = 'b', linestyle = "--", linewidth = 1)
 plt.show()
 
-#복수개의 라인을 한개로(점)
-t = np.arange(0., 5., 0.2)
-plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
-plt.show()
 
-#sin과 cos 그래프 2개
-X = np.arange(0, 3 * np.pi, 0.1)
-y_sin = np.sin(X)
-y_cos = np.cos(X)
-plt.subplot(2, 1, 1)
-plt.plot(X, y_sin)
-plt.title('sine')
-plt.subplot(2, 1, 2)
-plt.plot(X, y_cos)
-plt.title('cosine')
-plt.show()
 
-#격자, 선 속성, 격자 간격
-X = np.linspace(-1.5, 1.5, 100)
-F1 = X ** 2
-F2 = X ** 3
-ax = plt.axes()
-ax.xaxis.set_major_locator(ticker.MultipleLocator(0.3))
-ax.xaxis.set_minor_locator(ticker.MultipleLocator(0.1))
-plt.plot(X, F1, color='m', linestyle='dotted', linewidth=2.)
-plt.plot(X, F2, c='#0000FF', ls='dashed', lw=1.)
-plt.grid(True, which='both', c='0.5', ls='--', lw=0.5)
-plt.show()
+def incsearch(func, xmin, xmax, ns):
+    # x 범위 설정
+    x = np.linspace(xmin, xmax, ns)
+    # 주어진 함수 실행 배열 세팅
+    f = func(x)
+    # 구간값의 개수
+    nb = 0
+    # 구간값을 보관할 배열
+    xb = []
+
+    # 0부터 설정한 최고값까지 반복
+    for k in np.arange(np.size(x) - 1):
+        # f(x), f(x+1) 부호 다르면
+        if np.sign(f[k]) != np.sign(f[k+1]):
+            # 개수 추가
+            nb += 1
+            # 여기부터 여기까지
+            xb.append(x[k])
+            xb.append(x[k+1])
+    # 구간 개수, 구간 배열
+    return nb, xb
+
+print("[!] 1. Incremental Search\n")
+
+fx = lambda x: np.sin(10+x) + np.cos(3*x)
+nb, xb = incsearch(fx, 3, 6, 50)
+
+# 구간 갯수
+print("[+] Number of Brackets =", nb)
+# 구간들
+print("[+] Root Interval =", xb)
 """
+
+def bisect(func, xl, xu, es = 1.0e-4, maxit = 100):
+    # f(최저값)과 x(최고값)을 곱함
+    test = func(xl) * func(xu)
+
+    if (test > 0):
+        print("No sign change")
+        return [], [], [], []
+
+    iter = 0
+
+    xr = xl
+    ea = 100
+
+    while True:
+        xroid = xr
+        xr = float((xl + xu) / 2)
+        iter += 1
+
+        print("iter: ",iter)
+        print("xr: ",xr)
+
+        if xr != 0:
+            ea = np.abs((xr-xroid)/xr) * 100
+
+        test = func(xl) * func(xr)
+
+        if test < 0:
+            xu = xr
+        elif test > 0:
+            xl = xr
+        else:
+            ea = 0
+
+        if (ea <= es or iter >= maxit):
+            break;
+
+    root = xr
+    fx = func(xr)
+
+    return root, fx, ea, iter
+
+print("[!] 2. Bisection\n")
+fm = lambda m: np.sqrt(9.81 * m / 0.25) * np.tanh(np.sqrt(9.81 * 0.25 / m) * 4) - 36
+root, fx, ea, iter = bisect(fm, 40, 200, 0.001, 50)
+print("[+] root:", root)
+print("[+] f(root):", fx, "(Must Be Zero)")
+print("[+] Estimated Error:", ea, "(Must Be Zero Error)")
+print("[+] Iterated Number to Find Root:", iter)
