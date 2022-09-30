d406777463f5eb517992ae3bde5922cd3f37c523
Bincher <98527148+Bincher@users.noreply.github.com>
2022-09-30 17:15:55 +0900

    공수5주차 - 과제1-1

--- main.py
+++ main.py
@@ -4,55 +4,11 @@ import random as rd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
-"""
-cd = 0.25
-g = 9.81
-v = 36
-t = 4
-mp = np.linspace(50, 200, 10)
-fp = np.sqrt(g*mp/cd) * np.tanh(np.sqrt(g*cd/mp)*t) - v
+#xl : 계산할 x의 최저값
+#xu : 계산할 x의 최고값
+#ex :오차 범위
 
-plt.title("m graph")
-plt.plot(mp, fp)
-plt.grid(color = 'b', linestyle = "--", linewidth = 1)
-plt.show()
-
-
-
-def incsearch(func, xmin, xmax, ns):
-    # x 범위 설정
-    x = np.linspace(xmin, xmax, ns)
-    # 주어진 함수 실행 배열 세팅
-    f = func(x)
-    # 구간값의 개수
-    nb = 0
-    # 구간값을 보관할 배열
-    xb = []
-
-    # 0부터 설정한 최고값까지 반복
-    for k in np.arange(np.size(x) - 1):
-        # f(x), f(x+1) 부호 다르면
-        if np.sign(f[k]) != np.sign(f[k+1]):
-            # 개수 추가
-            nb += 1
-            # 여기부터 여기까지
-            xb.append(x[k])
-            xb.append(x[k+1])
-    # 구간 개수, 구간 배열
-    return nb, xb
-
-print("[!] 1. Incremental Search\n")
-
-fx = lambda x: np.sin(10+x) + np.cos(3*x)
-nb, xb = incsearch(fx, 3, 6, 50)
-
-# 구간 갯수
-print("[+] Number of Brackets =", nb)
-# 구간들
-print("[+] Root Interval =", xb)
-"""
-
-def bisect(func, xl, xu, es = 1.0e-4, maxit = 100):
+def bisect(func, xl, xu, es = 2, maxit = 100):
     # f(최저값)과 x(최고값)을 곱함
     test = func(xl) * func(xu)
 
@@ -85,6 +41,49 @@ def bisect(func, xl, xu, es = 1.0e-4, maxit = 100):
         else:
             ea = 0
 
+        if (ea < es or iter >= maxit):
+            break;
+
+    root = xr
+
+    fx = func(xr)
+
+    return root, fx, ea, iter
+
+def false_position(func, xl, xu, es = 1.0e-4, maxit = 100):
+    # f(최저값)과 x(최고값)을 곱함
+    test = func(xl) * func(xu)
+
+    if (test > 0):
+        print("No sign change")
+        return [], [], [], []
+
+    iter = 0
+
+    xr = xl
+    ea = 100
+
+    while True:
+        xroid = xr
+
+        xr = xu - (func(xu) * (xl - xu)) / (func(xl) - func(xu))
+        iter += 1
+
+        print("iter: ",iter)
+        print("xr: ",xr)
+
+        if xr != 0:
+            ea = np.abs((xr-xroid)/xr) * 100
+
+        test = func(xl) * func(xr)
+
+        if test < 0:
+            xu = xr
+        elif test > 0:
+            xl = xr
+        else:
+            ea = 0
+
         if (ea <= es or iter >= maxit):
             break;
 
@@ -93,6 +92,15 @@ def bisect(func, xl, xu, es = 1.0e-4, maxit = 100):
 
     return root, fx, ea, iter
 
+print("[!] 5.1) 번지점프, 이분법, 근사 상대오차가 2%이하로")
+fc = lambda c: np.sqrt((9.81*80) / c) * np.tanh(np.sqrt(9.81 * c / 80) * 4) - 36
+root, fx, ea, iter = bisect(fc, 0.1, 0.2)
+print("[+] root:", root)
+print("[+] f(root):", fx, "(Must Be Zero)")
+print("[+] Estimated Error:", ea, "(Must Be Zero Error)")
+print("[+] Iterated Number to Find Root:", iter)
+
+"""
 print("[!] 2. Bisection\n")
 fm = lambda m: np.sqrt(9.81 * m / 0.25) * np.tanh(np.sqrt(9.81 * 0.25 / m) * 4) - 36
 root, fx, ea, iter = bisect(fm, 40, 200, 0.001, 50)
@@ -100,3 +108,14 @@ print("[+] root:", root)
 print("[+] f(root):", fx, "(Must Be Zero)")
 print("[+] Estimated Error:", ea, "(Must Be Zero Error)")
 print("[+] Iterated Number to Find Root:", iter)
+
+print("=========================")
+print("[!] 3. false_position\n")
+fm2 = lambda n: pow(n, 10) - 1
+root2, fx2, ea2, iter2 = false_position(fm2, 0, 1.3, 0.35, 100)
+print("[+] root:", root2)
+print("[+] f(root):", fx2, "(Must Be Zero)")
+print("[+] Estimated Error:", ea2, "(Must Be Zero Error)")
+print("[+] Iterated Number to Find Root:", iter2)
+"""
+
