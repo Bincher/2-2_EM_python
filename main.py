8972de0cf8f9bc9479c3b2b0ed1d526cf669df2d
Bincher <98527148+Bincher@users.noreply.github.com>
2022-10-12 22:19:41 +0900

    공수6주차 - 과제완료!

--- main.py
+++ main.py
@@ -5,37 +5,40 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from scipy.optimize import fsolve
 
-def func(x):
-    return (x*x - 9)
+def Newton_Raphson(func, dfunc, xr, es = 5, maxit = 300):
 
-def Newton_Raphson(func, dfunc, xr, es = 0.0001, maxit = 50):
     iter = 0
+
     while(1):
         xrold = xr
-        xr = xr - np.feval(func, xr)/np.feval(dfunc, xr)
+        xr = xr - func(xr)/dfunc(xr)
         iter += 1
+
         if xr != 0:
-            ea = abs((xr - xrold)/xr) * 100
+            ea = (abs((xr - xrold)/xr) * 100)
+
         if ea <= es or iter >= maxit:
             break
     root = xr
 
     return root, ea, iter
 
+x = np.linspace(-5, 5, 20)
+fp = -2*pow(x,6) - 1.5*pow(x,4) + 10*x + 2
+plt.title("m graph")
+plt.plot(x, fp)
+plt.grid(color = 'b', linestyle = "--", linewidth = 1)
+plt.show()
+#fx = lambda x: x**3 - 6*(x**2) + 11*x - 6.1
+#fx2 = lambda x: 3*(x**2) - 12*x + 11
+#fx = lambda x: -2*pow(x,6) - 1.5*pow(x,4) + 10*x + 2
+#fx2 = lambda x: -12*pow(x,5) - 6*pow(x,3) + 10
 
-x = fsolve(func,-4)
-print(x)
-x = fsolve(func, 4)
-print(x)
-x = fsolve(func, 0)
-print(x)
-
-"""
 print("[!] 5. Newton_Raphson")
-fx = lambda x: (2.78)**(-x) - x
-fx2 = lambda x: (-2.78)**(-x) - 1
-root, ea, iter = Newton_Raphson(fx, fx2, 0)
+fx = lambda x: -12*pow(x,5) - 6*pow(x,3) + 10
+fx2 = lambda x: -60*pow(x,4) - 18*pow(x,2)
+root, ea, iter = Newton_Raphson(fx, fx2, 1)
 print("[+] root:", root)
-print("[+] Estimated Error:", ea, "(Must Be Zero Error)")
+print("[+] Estimated Error:", format(ea, '.5f'), "(Must Be Zero Error)")
 print("[+] Iterated Number to Find Root:", iter)
-"""
+print("극점이 최대값일테니... :",-2*pow(root,6) - 1.5*pow(root,4) + 10*root + 2)
