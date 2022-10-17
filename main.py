2da1544cd94c0b5b3c22b293068091c695635c37
Bincher <98527148+Bincher@users.noreply.github.com>
2022-10-17 11:13:23 +0900

    공수7주차 - 8장(미완)

--- main.py
+++ main.py
@@ -1,44 +1,18 @@
-import math
 import numpy as np
-import random as rd
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
-from scipy.optimize import fsolve
 
-def Newton_Raphson(func, dfunc, xr, es = 5, maxit = 300):
+K = np.array([[150, -100, 0], [-100, 150, -50], [0, -50, 50]])
+mg = np.array([588.6, 686.7, 784.8])
 
-    iter = 0
+x1 = np.linalg.inv(K) * mg
+print(x1)
 
-    while(1):
-        xrold = xr
-        xr = xr - func(xr)/dfunc(xr)
-        iter += 1
+x2 = np.dot(np.linalg.inv(K), mg)
+print(x2)
 
-        if xr != 0:
-            ea = (abs((xr - xrold)/xr) * 100)
+x = np.linalg.inv(K)
 
-        if ea <= es or iter >= maxit:
-            break
-    root = xr
+print(x)
 
-    return root, ea, iter
-
-x = np.linspace(-5, 5, 20)
-fp = -2*pow(x,6) - 1.5*pow(x,4) + 10*x + 2
-plt.title("m graph")
-plt.plot(x, fp)
-plt.grid(color = 'b', linestyle = "--", linewidth = 1)
-plt.show()
-#fx = lambda x: x**3 - 6*(x**2) + 11*x - 6.1
-#fx2 = lambda x: 3*(x**2) - 12*x + 11
-#fx = lambda x: -2*pow(x,6) - 1.5*pow(x,4) + 10*x + 2
-#fx2 = lambda x: -12*pow(x,5) - 6*pow(x,3) + 10
-
-print("[!] 5. Newton_Raphson")
-fx = lambda x: -12*pow(x,5) - 6*pow(x,3) + 10
-fx2 = lambda x: -60*pow(x,4) - 18*pow(x,2)
-root, ea, iter = Newton_Raphson(fx, fx2, 1)
-print("[+] root:", root)
-print("[+] Estimated Error:", format(ea, '.5f'), "(Must Be Zero Error)")
-print("[+] Iterated Number to Find Root:", iter)
-print("극점이 최대값일테니... :",-2*pow(root,6) - 1.5*pow(root,4) + 10*root + 2)
+xi = np.array([20, 40, 60])
+xf = x + xi
+print(xf)
