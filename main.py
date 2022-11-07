bfa98eb1c31589634b887611e89f8b94a9d460df
김성빈 <catsb21@gmail.com>
2022-11-07 10:48:15 +0900

    공수 10주차 - Cholesky 분해법

--- main.py
+++ main.py
@@ -1,76 +1,24 @@
 import numpy as np
 from scipy.linalg import lu
-from numpy.linalg import inv
+from numpy.linalg import inv, cholesky
 
-def lu(A):
-    # Get the number of rows
-    n = A.shape[0]
+A = np.array([[6, 15, 55],[15, 55, 225], [55, 225, 979]])
+b = np.array([[np.sum(A[0])], [np.sum(A[1])], [np.sum(A[2])]])
 
-    U = A.copy()
-    L = np.eye(n, dtype=np.double)
+print("np.array([[np.sum(A[0])], [np.sum(A[1])], [np.sum(A[2])]])")
+print(b)
 
-    # Loop over rows
-    for i in range(n):
-        # Eliminate entries below i with row operations
-        # on U and reverse the row operations to
-        # manipulate L
-        factor = U[i + 1:, i] / U[i, i]
-        L[i + 1:, i] = factor
-        U[i + 1:] -= factor[:, np.newaxis] * U[i]
+U = cholesky(A)
+print("cholesky(A) -> U.T")
+print(U.T)
 
-    return L, U
+print("np.dot(U, U.T)")
+print(np.dot(U, U.T))
 
-
-def plu(A):
-    # Get the number of rows
-    n = A.shape[0]
-
-    # Allocate space for P, L, and U
-    U = A.copy()
-    L = np.eye(n, dtype=np.double)
-    P = np.eye(n, dtype=np.double)
-
-    # Loop over rows
-    for i in range(n):
-
-        # Permute rows if needed
-        for k in range(i, n):
-            if ~np.isclose(U[i, i], 0.0):
-                break
-            U[[k, k + 1]] = U[[k + 1, k]]
-            P[[k, k + 1]] = P[[k + 1, k]]
-
-        # Eliminate entries below i with row
-        # operations on U and #reverse the row
-        # operations to manipulate L
-        factor = U[i + 1:, i] / U[i, i]
-        L[i + 1:, i] = factor
-        U[i + 1:] -= factor[:, np.newaxis] * U[i]
-
-    return P, L, U
-
-
-A = np.array([[3, -0.1, -0.2], [0.1, 7, -0.3], [0.3, -0.2, 10]])
-b = np.array([[7.85], [-19.3], [71.4]])
-
-p, l, u = plu(A)
-
-print("l")
-print(l)
-
-print("u")
-print(u)
-
-print("l * u (x)")
-print(l*u)
-
-print("l * u (o)")
-print(np.dot(l, u))
-
-d = np.dot(inv(l), b)
-print("전진대입")
+d = np.dot(inv(U),b)
+print("np.dot(inv(U,b))")
 print(d)
 
-x = np.dot(inv(u), d)
-print("후진대입")
-print(x)
+x = np.dot(inv(U.T), d)
+print("np.dot(inv(U.T), d)")
+print(x);
