341a7e7e00fb1bf74ba10495aba1db9b7edaa781
김성빈 <catsb21@gmail.com>
2022-11-10 00:19:05 +0900

    공수 10주차 - 11장 과제

--- main.py
+++ main.py
@@ -1,185 +1,122 @@
 import numpy as np
-from scipy.linalg import lu
-from numpy.linalg import inv, cholesky
-
-def gauss_naive(A, b):
-
-    n = len(A)
-    if b.size != n:
-        raise ValueError("Matrix A must be square", b.size, n)
-
-    for pivot_row in range(n-1):
-        for row in range(pivot_row + 1, n):
-            factor = A[row][pivot_row]/A[pivot_row][pivot_row]
-
-            A[row][pivot_row] = factor
-            for col in range(pivot_row + 1, n):
-                A[row][col] = A[row][col] - factor * A[pivot_row][col]
-
-            b[row] = b[row] - factor * b[pivot_row]
-
-    x = np.zeros(n)
-    k = n-1
-    x[k-1] = A[k-1][k] / A[k-1][k-1]
-
-    for i in range(k, -1, -1):
-        x[i] = (b[i] - np.dot(A[i, i+1:],x[i+1:]))/A[i,i]
-
-    return x
-
-def gauss_pivot(A, b):
-
-    n = len(A)
-    if b.size != n:
-        raise ValueError("Matrix A must be square", b.size, n)
-
-    for k in range(n-1):
-        maxindex = abs(A[k: ,k]).argmax() + k
-        if A[maxindex, k] == 0:
-            raise ValueError("Matrix is singular.")
-
-        if maxindex != k:
-            A[[k, maxindex]] = A[[maxindex, k]]
-            b[[k, maxindex]] = b[[maxindex, k]]
-
-        for row in range(k+1, n):
-            factor = A[row,k]/A[row,k]
-
-            A[row][k] = factor
-            for col in range(k + 1, n):
-                A[row][col] = A[row][col] - factor*A[k][col]
-            b[row] = b[row] - factor*b[k]
-def lu(A):
-    # Get the number of rows
-    n = A.shape[0]
-
-    U = A.copy()
-    L = np.eye(n, dtype=np.double)
-
-    # Loop over rows
-    for i in range(n):
-        # Eliminate entries below i with row operations
-        # on U and reverse the row operations to
-        # manipulate L
-        factor = U[i + 1:, i] / U[i, i]
-        L[i + 1:, i] = factor
-        U[i + 1:] -= factor[:, np.newaxis] * U[i]
-
-    return L, U
-
-def plu(A):
-    # Get the number of rows
-    n = A.shape[0]
-
-    # Allocate space for P, L, and U
-    U = A.copy()
-    L = np.eye(n, dtype=np.double)
-    P = np.eye(n, dtype=np.double)
-
-    # Loop over rows
-    for i in range(n):
-
-        # Permute rows if needed
-        for k in range(i, n):
-            if ~np.isclose(U[i, i], 0.0):
-                break
-            U[[k, k + 1]] = U[[k + 1, k]]
-            P[[k, k + 1]] = P[[k + 1, k]]
-
-        # Eliminate entries below i with row
-        # operations on U and #reverse the row
-        # operations to manipulate L
-        factor = U[i + 1:, i] / U[i, i]
-        L[i + 1:, i] = factor
-        U[i + 1:] -= factor[:, np.newaxis] * U[i]
-
-    return P, L, U
+from numpy import linalg as LA
 
 """
-A = np.array([[7.0, 2.0, -3.0], [2.0, 5.0, -3.0], [1.0, -1.0, -6.0]])
-b = np.array([[-12.0], [-20.0], [-26.0]])
+A = np.array([[10.0, 2.0, -1.0], [-3.0, -6.0, 2.0], [1.0, 1.0, 5.0]])
+b = np.array([[27.0],[-61.5],[-21.5]])
 
-
-p, l, u = plu(A)
-
-
-print("l")
-print(l)
-print("u")
-print(u)
-print("L*U")
-print(np.dot(l,u))
+print("a")
+print(a)
+# 스펙트랄(p=2) 놈에 근거한 조건수
+print("\nLA.cond(a)")
+print(LA.cond(a))
+print("\nLA.cond(a, 'fro')")
+print(LA.cond(a, 'fro'))
+#행렬의 행-합 놈
+print("\nLA.cond(a, np.inf)")
+print(LA.cond(a, np.inf))
+print("\nLA.cond(a, -np.inf)")
+print(LA.cond(a, -np.inf))
+print("\nLA.cond(a, 1)")
+print(LA.cond(a, 1))
+print("\nLA.cond(a, -1)")
+print(LA.cond(a, -1))
+print("\nLA.cond(a, 2)")
+print(LA.cond(a, 2))
+print("\nLA.cond(a, -2)")
+print(LA.cond(a, -2))
+print("\nmin(LA.svd(a, compute_uv=False))*min(LA.svd(LA.inv(a), compute_uv=False))")
+print(min(LA.svd(a, compute_uv=False))*min(LA.svd(LA.inv(a), compute_uv=False)))
 """
 
 """
-print("l")
-print(l)
+A = np.array([[10.0, 2.0, -1.0], [-3.0, -6.0, 2.0], [1.0, 1.0, 5.0]])
+b = np.array([[27.0],[-61.5],[-21.5]])
 
-print("u")
-print(u)
-
-print("l * u (o)")
-print(np.dot(l, u))
-
-b2 = np.array([12.0, 18.0, -6.0])
-d = np.dot(inv(l), np.transpose(b2))
-print("전진대입")
-print(d)
-
-x = np.dot(inv(u), d)
-print("후진대입")
-print(x)
+Ad = LA.linalg.inv(A)
+print("역행렬")
+print(Ad)
+print("[A][A]^-1")
+print(LA.linalg.dot(A,Ad))
 """
 
 """
-A = np.array([[8.0, 20.0, 16.0],[20.0, 80.0, 50.0], [16.0, 50.0, 60.0]])
-b = np.array([[100.0], [250.0], [100.0]])
+A = np.array([[-8.0, 1.0, -2.0], [-2.0, -6.0, -1.0], [-3.0, -1.0, 7.0]])
+b = np.array([[-20.0],[-38.0],[-34.0]])
 
-print("np.array([[np.sum(A[0])], [np.sum(A[1])], [np.sum(A[2])]])")
+Ad = LA.linalg.inv(A)
+print("역행렬")
+print(Ad)
+"""
+
+"""
+a = np.array([[8.0, 2.0, -10.0], [-9.0, 1.0, 3.0], [15.0, -1.0, 6.0]])
+b = np.array([[8.0/-10.0, 2.0/-10.0, -10.0/-10.0], [-9.0/-9.0, 1.0/-9.0, 3.0/-9.0], [15.0/15.0, -1.0/15.0, 6.0/15.0]])
+
+print("a")
+print(a)
+
+print("b")
 print(b)
 
-U = cholesky(A)
-#print("U")
-#print(U)
+print("Af")
+print(LA.norm(b,'fro'))
 
-print("cholesky(A) -> U.T")
-print(U.T)
+print("At")
+print(LA.norm(b, 1))
 
-print("np.dot(U, U.T)")
-print(np.dot(U, U.T))
+print("Ainf")
+print(LA.norm(b, np.inf))
 
-d = np.dot(inv(U),b)
-print("np.dot(inv(U,b))")
-print(d)
+# 스펙트랄(p=2) 놈에 근거한 조건수
+print("\nLA.cond(b) 스펙트랄(p=2) 놈에 근거한 조건수")
+print(LA.cond(b))
 
-x = np.dot(inv(U.T), d)
-print("np.dot(inv(U.T), d)")
-print(x)
+# 행렬의 행-합 놈
+print("\nLA.norm(b, np.inf) 행렬의 행-합 놈")
+print(LA.norm(b, np.inf))
+
+# 역행렬의 햅-합 놈
+print("\nLA.norm(LA.inv(b), np.inf) 역행렬의 햅-합 놈")
+print(LA.norm(LA.inv(b), np.inf))
+
+# 행-합 놈으로 구한 조건수
+print("\nLA.cond(b, np.inf) 행-합 놈으로 구한 조건수")
+print(LA.cond(b, np.inf))
+
+t1 = LA.norm(b, np.inf)
+t2 = LA.norm(LA.inv(b), np.inf)
+
+print("\nLA.norm(LA.inv(b), np.inf) * LA.norm(b, np.inf)")
+print(t1*t2)
 """
 
-
-A = np.array([[2.0, -1.0, 0.0],[-1.0, 2.0, -1.0], [0.0, -1.0, 2.0]])
-
-U = cholesky(A)
-#print("U")
-#print(U)
-
-print("A")
-print(A)
-
-print("cholesky(A) -> U.T")
-print(U.T)
-
-print("np.dot(U, U.T)")
-print(np.dot(U, U.T))
-
 """
-A = np.array([[9.0, 0.0, 0.0],[0.0, 25.0, 0.0], [0.0, 0.0, 16.0]])
+A = np.array([[-8.0, 1.0, -2.0], [-2.0, -6.0, -1.0], [-3.0, -1.0, 7.0]])
+B = np.array([[15.0, -3.0, -1.0], [-3.0, 18.0, -6.0], [-4.0, -1.0, 12.0]])
 
-U = cholesky(A)
-#print("U")
-#print(U)
+print("Af")
+print(LA.norm(A,'fro'))
 
-print("cholesky(A) -> U.T")
-print(U.T)
+print("Ainf")
+print(LA.norm(A, np.inf))
+
+print("Bf")
+print(LA.norm(B,'fro'))
+
+print("Binf")
+print(LA.norm(B, np.inf))
 """
+
+x1 = 4.0
+x2 = 2.0
+x3 = 7.0
+A = np.array([[x1**2, x1, 1.0], [x2**2, x2, 1.0], [x3**2, x3, 1.0]])
+
+print("행-합 놈에 근거한 조건수 계산")
+print(LA.cond(A, np.inf))
+
+print("\n스펙트랄 놈에 근거한 조건수")
+print(LA.cond(A))
+print("LA.cond(A, 'fro')")
+print(LA.cond(A,'fro'))
