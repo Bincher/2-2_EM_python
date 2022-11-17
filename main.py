08d8f3387848c2f477d4a539b5b5575d77f88799
seongbin <catsb21@gmail.com>
2022-11-17 23:17:36 +0900

    공수 11주차 - 12장 과제 ing

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import numpy as np
 from numpy import linalg as LA
 
+def seidel(a, x, b):
+    # Finding length of a(3)
+    n = len(a)
+    # for loop for 3 times as to calculate x, y , z
+    for j in range(0, n):
+        # temp variable d to store b[j]
+        d = b[j]
+
+        # to calculate respective xi, yi, zi
+        for i in range(0, n):
+            if (j != i):
+                d -= a[j][i] * x[i]
+        # updating the value of our solution
+        x[j] = d / a[j][j]
+    # returning our updated solution
+    return x
+
 def gaussSeidel(A, b, x, N, tol):
     maxIterations = 1000000
     xprev = [0.0 for i in range(N)]
@@ -29,15 +46,23 @@ def gaussSeidel(A, b, x, N, tol):
             return
     print("Doesn't converge.")
 
-matrix2 = [[3.0, 1.0], [2.0, 6.0]]
-vector2 = [5.0, 9.0]
-guess = [0.0, 0.0]
+"""
+a = [[6.0, -1.0], [3.0, 8.0]]
+b = [5.0, 11.0]
+x = [0.0, 0.0]
 
-matrix3 = [[9.0, -3.0], [-2.0, 8.0]]
-vector3 = [6.0, -4.0]
+for i in range(0, 3):
+    x = seidel(a, x, b)
+    #print each time the updated solution
+    print(x)
+    print("et1 = ", (abs(1 - x[0]) / 1) * 100, "%")
+    print("et2 = ", (abs(1 - x[1]) / 1) * 100, "%")
+"""
 
-matrix4 = [[1.0, -3.0], [-2.0, 8.0]]
+"""
+a = [[10.0, 2.0, -1.0], [-3.0, -6.0, 2.0], [1.0, 1.0, 5.0]]
+b = [27.0, -61.5, -21.5]
+x = [0.0, 0.0, 0.0]
 
-gaussSeidel(matrix2, vector2, guess, 2, 0.00000000000001)
-gaussSeidel(matrix3, vector3, guess, 2, 0.00000000000001)
-gaussSeidel(matrix4, vector3, guess, 2, 0.00000000000001)
+gaussSeidel(a, b, x, 3, 5)
+"""
