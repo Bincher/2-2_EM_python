de7d9303e656830887f67b9f727b69fe98ea3211
seongbin <catsb21@gmail.com>
2022-11-28 20:41:42 +0900

    공수 13주차 - 미완

--- main.py
+++ main.py
@@ -1,17 +1,44 @@
-import matplotlib
+from scipy.interpolate import lagrange
+from numpy.polynomial.polynomial import Polynomial
+import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib.pyplot as pit
-from numpy import linalg as LA
 
-A = np.array([[90000, 300, 1], [160000, 400, 1], [250000, 500, 1]])
-print(LA.cond(A))
+def Newint(x, y, xx):
+    n = len(x)
+    if (len(y) != n):
+        print("ERROR : x and y must be same length")
+        return
+    b = np.zeros((n,n))
+    b[:,1] = y[:]
+    for j in range(2,n):
+        for i in range(1, n-j+1):
+            b[i,j] = (b[i+1, j-1]-b[i,j-1])/x[i+j-1]-x[i]
+    xt = 1
+    yint = b[1,1]
+    for j in range(1, n-1):
+        xt = xt * (xx-x[j])
+        yint = yint + b[1,j+1]*xt
+    return yint
+def Lagrange(x, y, xx):
+    n = len(x)
+    if (len(y) != n):
+        print("ERROR : x and y must be same length")
+        return
+    s = 0
+    for i in range(1,n):
+        product = y[i]
+        for j in range(1,n):
+            if (i != j):
+                product = product * (xx - x[j]) / (x[i] - x[j])
+        s = s + product
+    return s
 
-T = np.array([300, 400, 500])
-density = np.array([0.616, 0.525, 0.457])
-p = np.polyfit(T, density, 2)
+x = np.array([1.0,4.0,6.0,5.0])
+y = np.log(x)
+print(Newint(x,y,2))
 
-print(p)
+T = np.array([-40.0, 0.0, 20.0, 50.0])
+d = np.array([1.52, 1.29, 1.2, 1.09])
+density = Lagrange(T, d, 15)
 
-#350도에서 밀도
-d = np.polyval(p, 350)
-print(d)
+print(density)
